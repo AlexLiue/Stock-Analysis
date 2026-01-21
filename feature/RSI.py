df0773acc6052ad940ff428f538898c3c04f0aaa
alex <alexliue@outlook.com>
2026-01-21 22:05:20 +0800

    init

---
+++ feature/RSI.py
@@ -0,0 +1,52 @@
+"""
+============================
+# -*- coding: utf-8 -*-
+# @Time    : 2026/1/21 21:33
+# @Author  : PcLiu
+# @FileName: RSI.py
+===========================
+
+特征计算： RSI-相对强弱指数 (Relative Strength Index)
+
+"""
+
+import vectorbt as vbt
+
+from data_dict import STOCK_ZH_A_HIST_DAILY_HFQ
+from data_dict.table import TABLE
+from utils.table_loader import load_table
+
+
+def RSI(df, index='日期', symbol='股票代码', value='收盘', window=14):
+    """
+    计算 [RSI-相对强弱指数 (Relative Strength Index)](feature/docs/RSI-相对强弱指数 (Relative Strength Index).md)
+    :param df: 输入数据
+    :param index:  日期列
+    :param symbol:  股票代码列（列名）
+    :param value:  计算的列值
+    :param window: 窗口大小
+    :return: RSI 相对强弱指数计算结果
+    """
+
+    """ Pivot（透视）：将长格式转为宽格式, 每只股票代码的数据作为一列 """
+    wide_df = df.pivot(index=index, columns=symbol, values=value)
+
+    """ 使用 vectorbt 并行计算每只股票的 RSI ,窗口大小 14 """
+    rsi = vbt.RSI.run(wide_df, window=window).rsi
+
+    """ 删除 index 索引, levels=0:删除第0层索引, axis=1: 删除列的索引, inplace=True:原地删除 """
+    return rsi.vbt.drop_levels(levels=0, axis=1, inplace=True)
+
+
+if __name__ == '__main__':
+    df = load_table(
+        table=TABLE.STOCK_ZH_A_HIST_DAILY_HFQ,
+        cols=[
+            STOCK_ZH_A_HIST_DAILY_HFQ.日期,
+            STOCK_ZH_A_HIST_DAILY_HFQ.股票代码,
+            STOCK_ZH_A_HIST_DAILY_HFQ.收盘]
+    )
+
+    rsi = RSI(df, index='日期', column='股票代码', value='收盘', window=14)
+
+    print(rsi)
--- utils/table_loader.py
+++ utils/table_loader.py
@@ -4,19 +4,30 @@ import pandas as pd
 
 from data_dict import STOCK_ZH_A_HIST_WEEKLY_QFQ
 from data_dict.table import TABLE
+from utils.config import get_cfg
+from utils.logger import get_logger
 from utils.tools import get_engine
+
 
 
 def load_table(
         table,
         cols,
-        where: str = None) -> pd.DataFrame:
+        where: str = None,
+        order: str = None) -> pd.DataFrame:
+    logger = get_logger("table_loader", get_cfg()["analysis-logging"]["filename"])
     conn = get_engine()
     query_columns = [
         f'"{k}"' if re.search(r"[\u4e00-\u9fa5]", str(k)) else k
         for k in cols
     ]
-    query = f"SELECT {', '.join(query_columns)} FROM {table}" if where is None else f"SELECT {', '.join(query_columns)} FROM {table} WHERE {where}"
+    query = f"SELECT {', '.join(query_columns)} FROM {table}"
+    if where is not None:
+        query = query + F" WHERE {where}"
+    if order is not None:
+        query = query + F" ORDER BY {order}"
+
+    logger.info(f"Load Table [{table}] With SQL [{query}]")
     return pd.read_sql(query, con=conn)
 
 
@@ -28,5 +39,8 @@ if __name__ == '__main__':
             STOCK_ZH_A_HIST_WEEKLY_QFQ.开盘,
             STOCK_ZH_A_HIST_WEEKLY_QFQ.收盘,
             STOCK_ZH_A_HIST_WEEKLY_QFQ.成交量
-        ])
+        ],
+        where=None,
+        order= STOCK_ZH_A_HIST_WEEKLY_QFQ.日期 + " ASC"
+    )
     print(stock_basic)
